4a29bd5787a76f96433aa4b22fc9d4e3dcc8356a
hyper-neutrino <hyperneutrino15@gmail.com>
2023-01-19 00:56:08 -0500

    honestly not sure how to make this work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Page from "$lib/Page.svelte";
+</script>
+
+<Page title="yes">Hello, World!</Page>
