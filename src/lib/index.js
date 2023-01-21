a603586fd820b8cece44623ed423501479c4d755
hyper-neutrino <hyperneutrino15@gmail.com>
2023-01-21 16:33:03 -0500

    add missing re-export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
+import Loading__SvelteComponent_ from "./Loading.svelte";
 import MetaTags__SvelteComponent_ from "./MetaTags.svelte";
 import Navbar__SvelteComponent_ from "./Navbar.svelte";
 
+export const Loading = Loading__SvelteComponent_;
 export const MetaTags = MetaTags__SvelteComponent_;
 export const Navbar = Navbar__SvelteComponent_;
