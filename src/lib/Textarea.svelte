02f86110626857232948033facb8ea719c3df64a
hyper-neutrino <hyperneutrino15@gmail.com>
2023-01-28 16:42:46 -0500

    improve textarea behavior

--- src/lib/Textarea.svelte
+++ src/lib/Textarea.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-    let value: string = $$props.value ?? "";
+    import { onMount } from "svelte";
+
+    function update() {
+        this.style.height = "0";
+        this.style.height = this.scrollHeight + "px";
+    }
+
+    let element: any;
+    onMount(() => update.bind(element)());
 </script>
 
-<textarea {...$$props} bind:value rows={value.split("\n").length} />
+<textarea {...$$props} on:input={update} bind:this={element} />
 
 <style lang="scss">
     textarea {
