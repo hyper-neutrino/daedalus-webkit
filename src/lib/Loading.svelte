381cd3580a1298568ee78a43bb8dbb9b9dc9f04d
hyper-neutrino <hyperneutrino15@gmail.com>
2023-01-28 13:59:23 -0500

    re-export loading spinner

--- src/lib/Loading.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let data: any = null;
-</script>
-
-{#if data !== null && data !== undefined}
-    <slot />
-{:else}
-    Loading...
-{/if}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
-import Loading__SvelteComponent_ from "./Loading.svelte";
+import LoadingSpinner__SvelteComponent_ from "./LoadingSpinner.svelte";
 import MetaTags__SvelteComponent_ from "./MetaTags.svelte";
 import Modal__SvelteComponent_ from "./Modal.svelte";
 import Navbar__SvelteComponent_ from "./Navbar.svelte";
 
 import { DiscordOAuth2 } from "./auth.js";
 
-export const Loading = Loading__SvelteComponent_;
+export const LoadingSpinner = LoadingSpinner__SvelteComponent_;
 export const MetaTags = MetaTags__SvelteComponent_;
 export const Modal = Modal__SvelteComponent_;
 export const Navbar = Navbar__SvelteComponent_;
